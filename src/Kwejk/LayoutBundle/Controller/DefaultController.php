aec671160fad0919d74fe2bb9f47ede0514117ce
Michał Nowaczyk <miko@MacBook-Pro-Micha-2.local>
2014-10-26 09:14:13 +0100

    Layout fix

--- src/Kwejk/LayoutBundle/Controller/DefaultController.php
+++ src/Kwejk/LayoutBundle/Controller/DefaultController.php
@@ -1,13 +1,10 @@
 <?php
-
 namespace Kwejk\LayoutBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('KwejkLayoutBundle:Default:index.html.twig', array('name' => $name));
-    }
+public function indexAction()
+{
+return $this->render('KwejkLayoutBundle:Default:index.html.twig', array());
+}
 }
